a5ff666d696eaedf163505891570bef06aadca57
Fwoopr <denizusluer2008@gmail.com>
2026-03-29 15:07:12 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+import sys
+from units import units
+
+def main():
+    if len(sys.argv) < 4 or len(sys.argv) > 5:
+        print("Usage: <format> <value> <unit> <target_unit>")
+        sys.exit(1)
+
+    if len(sys.argv) == 4:
+        unit_to = sys.argv[3].lower()
+        try:
+            value = float(sys.argv[1])
+        except ValueError:
+            print("Value must be a number.")
+            sys.exit(2)
+        unit_from = sys.argv[2].lower()
+        converted_value = convert(value, unit_to, unit_from)
+        print(f"{value} {unit_from} is equal to {converted_value} {unit_to}")
+
+    elif len(sys.argv) == 5:
+        format = sys.argv[1].lower()
+        unit_to = sys.argv[4].lower()
+        try:
+            value = float(sys.argv[2])
+        except ValueError:
+            print("Value must be a number.")
+            sys.exit(2)
+        unit_from = sys.argv[3].lower()
+        converted_value = convert(value, unit_to, unit_from)
+        if format == '-e':
+            print(f"{value:.2e} {unit_from} is equal to {converted_value:.2e} {unit_to}")
+        elif format == '-10x':
+            print(f"{tenx_format(value)} {unit_from} is equal to {tenx_format(converted_value)} {unit_to}")
+        else:
+            print('Invalid format. Use -e for scientific notation or -10x for 10-based notation.')
+
+def convert(value, unit_to, unit_from='m'):
+    for category in units:
+        if unit_from in units[category] and unit_to in units[category]:
+            return value * (units[category][unit_from] / units[category][unit_to])
+    raise ValueError(f"Conversion from {unit_from} to {unit_to} is not supported.")
+
+def tenx_format(value):
+    exponent = 0
+    if value >= 10:
+        while value >= 10:
+            value /= 10
+            exponent += 1
+    elif value < 1:
+        while value < 1:
+            value *= 10
+            exponent -= 1
+    return f"{value:.2f} x 10^{exponent}"
+    
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ units.py
@@ -0,0 +1,45 @@
+units = {
+    'length': {
+    # Base unit: meter (m)
+
+    # SI prefixes
+    'km': 1000.0,
+    'hm': 100.0,
+    'dam': 10.0,
+    'm': 1.0,
+    'cm': 0.01,
+    'mm': 0.001,
+    'dm': 0.1,
+    'μm': 1e-6,
+    'nm': 1e-9,
+    'pm': 1e-12,
+
+    # Imperial units
+    'in': 0.0254,
+    'ft': 0.3048,
+    'yd': 0.9144,
+    'mi': 1609.344,
+    },
+
+    'mass': {
+    # Base unit: gram (g)
+
+    # SI prefixes
+    'kg': 1000.0,
+    'hg': 100.0,
+    'dag': 10.0,
+    'g': 1.0,
+    'dg': 0.1,
+    'cg': 0.01,
+    'mg': 0.001,
+    'μg': 1e-6,
+    'ng': 1e-9,
+    'pg': 1e-12,
+
+    # Imperial units
+    'oz': 28.349523125,
+    'lb': 453.59237,
+    'ton': 907184.74,
+
+    }
+}
